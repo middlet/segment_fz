ec715c47573d626bfc3a0d2f1a9e0d1c615ac57b
happy@home <happy@theshire.(none)>
2012-03-17 10:15:42 +0000

    minor commit. i think the issue is that the array is not allocated correctly.

--- src/fzsegment.cpp
+++ src/fzsegment.cpp
@@ -36,16 +36,16 @@ image<rgb> *opencv_to_imagefz(const cv::Mat &im) {
 cv::Mat *imagefz_to_opencv(image<rgb> *im) {
     int width = im->width();
     int height = im->height();
-    cv::Mat *imout = new cv::Mat(width, height, CV_8UC3); 
-    for (int y=0; y<height; y++) {
-        for (int x=0; x<width; x++) {
-            cv::Vec3b pixel = imout->at<cv::Vec3b>(x, y);
-            pixel[0] = imRef(im, x, y).r;
-            pixel[1] = imRef(im, x, y).g;
-            pixel[2] = imRef(im, x, y).b;
-            imout->at<cv::Vec3b>(0,0) = pixel;
-        } // x
-    } // y
+    cv::Mat *imout = new cv::Mat(width, height, CV_8UC3, 0); 
+    //for (int y=0; y<height; y++) {
+    //    for (int x=0; x<width; x++) {
+    //        cv::Vec3b pixel = imout->at<cv::Vec3b>(x, y);
+    //        pixel[0] = imRef(im, x, y).r;
+    //        pixel[1] = imRef(im, x, y).g;
+    //        pixel[2] = imRef(im, x, y).b;
+    //        imout->at<cv::Vec3b>(0,0) = pixel;
+    //    } // x
+    //} // y
     return imout;
 }
 
