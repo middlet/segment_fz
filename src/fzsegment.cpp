a5073cef8b2845cea33b7771b890ebedf8ce0bc7
happy@home <happy@theshire.(none)>
2012-03-18 18:10:19 +0000

    fixed the copy from fz images to opencv imag code

--- src/fzsegment.cpp
+++ src/fzsegment.cpp
@@ -39,11 +39,11 @@ cv::Mat *imagefz_to_opencv(image<rgb> *im) {
     cv::Mat *imout = new cv::Mat(height, width, CV_8UC3, cv::Scalar(0,0,0)); 
     for (int y=0; y<height; y++) {
         for (int x=0; x<width; x++) {
-            cv::Vec3b pixel = imout->at<cv::Vec3b>(x, y);
+            cv::Vec3b pixel = imout->at<cv::Vec3b>(y, x);
             pixel[0] = imRef(im, x, y).r;
             pixel[1] = imRef(im, x, y).g;
             pixel[2] = imRef(im, x, y).b;
-            imout->at<cv::Vec3b>(0,0) = pixel;
+            imout->at<cv::Vec3b>(y,x) = pixel;
         } // x
     } // y
     
